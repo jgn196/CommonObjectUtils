9eebc3106dfb5de08c82f5a9ab5414750a3577a6
John Nash <john.nash@capgemini.com>
2014-08-20 13:43:27 +0000

    Implemented EqualsBuilder and its unit tests.

---
+++ CommonObjectUtils/EqualsBuilder.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonObjectUtils
+{
+    /// <summary>
+    /// A port of the Apache Commons EqualsBuilder utility class.
+    /// </summary>
+    public class EqualsBuilder
+    {
+        /// <summary>
+        /// The state of the equals tests.
+        /// <para>
+        /// This starts off as true and is updated by each Append method.
+        /// </para>
+        /// </summary>
+        private bool isEqual = true;
+
+        /// <summary>
+        /// Gets a value indicating whether the fields that have been checked were equal.
+        /// </summary>
+        public bool IsEquals
+        {
+            get
+            {
+                return isEqual;
+            }
+        }
+
+        /// <summary>
+        /// Compares two objects that implement <c>IEquatable</c> interface.
+        /// </summary>
+        /// <typeparam name="T">The type of the objects.</typeparam>
+        /// <param name="left">The left hand object.</param>
+        /// <param name="right">The right hand object.</param>
+        /// <returns>This EqualsBuilder for chaining calls.</returns>
+        public EqualsBuilder Append<T>(T left, T right) where T : IEquatable<T>
+        {
+            isEqual = isEqual && left.Equals(right);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Does a deep comparison of two arrays containing the same type.
+        /// </summary>
+        /// <typeparam name="T">The type the arrays contain.</typeparam>
+        /// <param name="left">The left hand array.</param>
+        /// <param name="right">The right hand array.</param>
+        /// <returns>This EqualsBuilder for chaining calls.</returns>
+        public EqualsBuilder Append<T>(T[] left, T[] right) where T : IEquatable<T>
+        {
+            return Append((IEnumerable<T>)left, (IEnumerable<T>)right);
+        }
+
+        /// <summary>
+        /// Does a deep comparison of two enumerable objects containing the same type.
+        /// </summary>
+        /// <typeparam name="T">The type the enumerable objects contain.</typeparam>
+        /// <param name="left">The left hand enumerable.</param>
+        /// <param name="right">The right hand enumerable.</param>
+        /// <returns>This EqualsBuilder for chaining calls.</returns>
+        public EqualsBuilder Append<T>(IEnumerable<T> left, IEnumerable<T> right) where T : IEquatable<T>
+        {
+            if (left == null || right == null)
+            {
+                isEqual = isEqual && left == right;
+            }
+            else if (left.Count() != right.Count())
+            {
+                isEqual = false;
+            }
+            else
+            {
+                var comparisons = left.Zip(right, (l, r) => l.Equals(r));
+
+                if (comparisons.Count(x => x) != left.Count())
+                {
+                    isEqual = false;
+                }
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the result of super.Equals().
+        /// </summary>
+        /// <param name="superEquals">The result of super.Equals().</param>
+        /// <returns>This EqualsBuilder for chaining calls.</returns>
+        public EqualsBuilder AppendSuper(bool superEquals)
+        {
+            isEqual = isEqual && superEquals;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Reset the EqualsBuilder so that it can be used again.
+        /// </summary>
+        public void Reset()
+        {
+            isEqual = true;
+        }
+    }
+}
---
+++ CommonObjectUtilsTests/EqualsBuilderTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+
+using CommonObjectUtils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonObjectUtilsTests
+{
+    /// <summary>
+    /// Tests the EqualsBuilder class.
+    /// </summary>
+    [TestClass]
+    public class EqualsBuilderTests
+    {
+        /// <summary>
+        /// Tests comparing objects.
+        /// </summary>
+        [TestMethod]
+        public void Append()
+        {
+            Assert.IsTrue(new EqualsBuilder().Append(true, true).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append(false, false).IsEquals);
+
+            Assert.IsFalse(new EqualsBuilder().Append(false, true).IsEquals);
+            Assert.IsFalse(new EqualsBuilder().Append(true, false).IsEquals);
+
+            Assert.IsFalse(new EqualsBuilder().Append(false, true).Append(true, true).IsEquals);
+
+            Assert.IsTrue(new EqualsBuilder().Append((byte)1, (byte)1).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append((short)1, (short)1).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append((int)1, (int)1).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append((long)1, (long)1).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append((float)1.0, (float)1.0).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append((double)1, (double)1).IsEquals);
+
+            Assert.IsTrue(new EqualsBuilder().Append((ushort)1, (ushort)1).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append((uint)1, (uint)1).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append((ulong)1, (ulong)1).IsEquals);
+
+            Assert.IsTrue(new EqualsBuilder().Append('c', 'c').IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append("foo", "foo").IsEquals);
+        }
+
+        /// <summary>
+        /// Tests comparing enumerable objects.
+        /// </summary>
+        [TestMethod]
+        public void AppendEnumerables()
+        {
+            Assert.IsTrue(new EqualsBuilder().Append((bool[])null, (bool[])null).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append(new bool[] { true }, new bool[] { true }).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append(new bool[] { false }, new bool[] { false }).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append(new bool[] { true, true }, new bool[] { true, true }).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append(new bool[] { false, false }, new bool[] { false, false }).IsEquals);
+            Assert.IsTrue(new EqualsBuilder().Append(new bool[0], new bool[0]).IsEquals);
+
+            Assert.IsTrue(new EqualsBuilder().Append((IEnumerable<bool>)new List<bool>() { true }, new List<bool>() { true }).IsEquals);
+
+            Assert.IsTrue(new EqualsBuilder().Append((IEnumerable<bool>)new HashSet<bool>() { true }, new HashSet<bool>() { true }).IsEquals);
+
+            Assert.IsFalse(new EqualsBuilder().Append(new bool[] { true }, null).IsEquals);
+            Assert.IsFalse(new EqualsBuilder().Append(null, new bool[] { true }).IsEquals);
+            Assert.IsFalse(new EqualsBuilder().Append(new bool[] { true }, new bool[] { true, true }).IsEquals);
+            Assert.IsFalse(new EqualsBuilder().Append(new bool[] { true, true }, new bool[] { true, false }).IsEquals);
+        }
+
+        /// <summary>
+        /// Tests the AppendSuper method.
+        /// </summary>
+        [TestMethod]
+        public void AppendSuper()
+        {
+            EqualsBuilder builder = new EqualsBuilder();
+
+            Assert.IsTrue(builder.IsEquals);
+
+            builder.AppendSuper(true);
+
+            Assert.IsTrue(builder.IsEquals);
+
+            builder.AppendSuper(false);
+
+            Assert.IsFalse(builder.IsEquals);
+        }
+
+        /// <summary>
+        /// Tests the Reset method.
+        /// </summary>
+        [TestMethod]
+        public void Reset()
+        {
+            EqualsBuilder builder = new EqualsBuilder();
+            builder.AppendSuper(false);
+
+            Assert.IsFalse(builder.IsEquals);
+
+            builder.Reset();
+
+            Assert.IsTrue(builder.IsEquals);
+        }
+    }
+}
