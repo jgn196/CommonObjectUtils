a276dd37316fd86ca1e01ee87fc927708a109193
John Nash <john.nash@capgemini.com>
2014-08-29 13:03:53 +0000

    Added Exception.IsOneOf()
    Added a new ExceptionExtensions class that holds the extension method
    IsOneOf.

---
+++ CommonObjectUtils/ExceptionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+using CuttingEdge.Conditions;
+
+namespace Capgemini.CommonObjectUtils
+{
+    /// <summary>
+    /// Holds extensions to the Exception class.
+    /// </summary>
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Checks if the exception is an instance of, sub-class of or implements one of a list of types.
+        /// </summary>
+        /// <param name="exception">The exception being checked.</param>
+        /// <param name="argsRest">
+        /// An array of types that the exception will be compared to.
+        /// </param>
+        /// <returns>True if the exception's type matched any of the types.</returns>
+        [SuppressMessage("Microsoft.Design", "CA1062", MessageId = "0", Justification = "Argument validated")]
+        public static bool IsOneOf(this Exception exception, params Type[] argsRest)
+        {
+            Condition.Requires(exception).IsNotNull();
+            Condition.Requires(argsRest).IsNotNull();
+
+            Type exceptionType = exception.GetType();
+
+            return argsRest.Any(arg => arg.IsAssignableFrom(exceptionType));
+        }
+    }
+}
---
+++ CommonObjectUtilsTests/ExceptionExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Capgemini.CommonObjectUtils.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capgemini.CommonObjectUtils.Tests
+{
+    /// <summary>
+    /// Tests the ExceptionExtensions class.
+    /// </summary>
+    [TestClass]
+    public class ExceptionExtensionsTests
+    {
+        /// <summary>
+        /// Tests passing various bad arguments to the IsOneOf method.
+        /// </summary>
+        [TestMethod]
+        public void ExceptionExtensions_IsOneOf_BadArgs()
+        {
+            new ErrorTester()
+                .Test(typeof(ArgumentNullException), () => ((Exception)null).IsOneOf(typeof(Exception)))
+                .Test(typeof(ArgumentNullException), () => new Exception().IsOneOf(null));
+        }
+
+        /// <summary>
+        /// Tests the IsOneOf method.
+        /// </summary>
+        [TestMethod]
+        public void ExceptionExtensions_IsOneOf()
+        {
+            Exception exception = new ArgumentException();
+
+            Assert.IsFalse(exception.IsOneOf());
+            Assert.IsFalse(exception.IsOneOf(typeof(ArgumentNullException)));
+            Assert.IsFalse(exception.IsOneOf(
+                typeof(InvalidOperationException), 
+                typeof(ArgumentNullException)));
+            Assert.IsFalse(exception.IsOneOf(typeof(uint)));
+
+            Assert.IsTrue(exception.IsOneOf(typeof(ArgumentException)));
+            Assert.IsTrue(exception.IsOneOf(typeof(Exception)));
+            Assert.IsTrue(exception.IsOneOf(
+                typeof(ArgumentNullException),
+                typeof(ArgumentException)));
+        }
+    }
+}
