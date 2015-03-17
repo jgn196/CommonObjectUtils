40f69507e6b6d62a47c7ba65ab682949c387b0d9
John Nash <john.nash@capgemini.com>
2015-03-17 20:55:55 +0000

    Added an in memory one-way pipe.

---
+++ CommonObjectUtils/MemoryPipe.cs
@@ -0,0 +1,60 @@
+﻿using CuttingEdge.Conditions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Capgemini.CommonObjectUtils
+{
+    /// <summary>
+    /// A one way in memory data pipe.
+    /// </summary>
+    public class MemoryPipe
+    {
+        private readonly List<byte> dataStream = new List<byte>();
+
+        /// <summary>
+        /// The size of the pipe in bytes.
+        /// </summary>
+        /// <remarks>
+        /// This is the total number of bytes that have been written to the pipe but haven't yet been
+        /// read from it.
+        /// </remarks>
+        public long Size { get { return dataStream.Count; } }
+
+        /// <summary>
+        /// Writes data to the pipe.
+        /// </summary>
+        /// <param name="data">The data to write to the pipe.</param>
+        public void Write(byte[] data)
+        {
+            Condition.Requires(data).IsNotNull();
+
+            dataStream.AddRange(data);
+        }
+
+        /// <summary>
+        /// Reads data from the pipe.
+        /// </summary>
+        /// <param name="count">The number of bytes to read from the pipe.</param>
+        /// <returns>An array containing the read bytes.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// This is thrown if the caller has requested more data than the pipe contains.
+        /// </exception>
+        public byte[] Read(int count)
+        {
+            if (count > Size)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "count",
+                    "The count argument (" + count + ") was larger than the pipe size (" + Size + ").");
+            }
+
+            var result = dataStream.Take(count).ToArray();
+            dataStream.RemoveRange(0, count);
+            dataStream.Capacity = dataStream.Count;
+            return result;
+        }
+    }
+}
---
+++ CommonObjectUtilsTests/MemoryPipeTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capgemini.CommonObjectUtils.Tests
+{
+    /// <summary>
+    /// Tests the MemoryPipe class.
+    /// </summary>
+    [TestClass]
+    public class MemoryPipeTests
+    {
+        private const int Kilobyte = 1024;
+        private readonly byte[] testData = new byte[Kilobyte];
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var random = new Random(42);
+            random.NextBytes(testData);
+        }
+
+        /// <summary>
+        /// Tests that an empty pipe reports its size as zero.
+        /// </summary>
+        [TestMethod]
+        public void MemoryPipe_Size_EmptyPipe_SizeIs0()
+        {
+            Assert.AreEqual(0, BuildEmptyPipe().Size);
+        }
+
+        private static MemoryPipe BuildEmptyPipe()
+        {
+            return new MemoryPipe();
+        }
+
+        /// <summary>
+        /// Tests that an empty pipe's size increases when data is written to it.
+        /// </summary>
+        [TestMethod]
+        public void MemoryPipe_Write1k_SizeIs1k()
+        {
+            var pipe = BuildEmptyPipe();
+
+            pipe.Write(new byte[Kilobyte]);
+
+            Assert.AreEqual(Kilobyte, pipe.Size);
+        }
+
+        /// <summary>
+        /// Tests that you can't read anything from an empty pipe.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void MemoryPipe_Read1kFromEmptyPipe_Throws()
+        {
+            BuildEmptyPipe().Read(Kilobyte);
+        }
+
+        /// <summary>
+        /// Tests that you get the same 1k of data out that you put in.
+        /// </summary>
+        [TestMethod]
+        public void MemoryPipe_Read1kFrom1kPipe()
+        {
+            CollectionAssert.AreEqual(testData, Build1KPipe().Read(Kilobyte));
+        }
+
+        private MemoryPipe Build1KPipe()
+        {
+            var pipe = BuildEmptyPipe();
+
+            pipe.Write(testData);
+            
+            return pipe;
+        }
+
+        /// <summary>
+        /// Tests that the pipe doesn't hang on to memory it doesn't need.
+        /// </summary>
+        [TestMethod]
+        public void MemoryPipe_DoesntLeakMemory()
+        {
+            var pipe = Build1KPipe();
+
+            long memoryMark = GC.GetTotalMemory(true);
+
+            pipe.Read(Kilobyte);
+
+            Assert.IsTrue(GC.GetTotalMemory(true) < memoryMark);
+        }
+    }
+}
